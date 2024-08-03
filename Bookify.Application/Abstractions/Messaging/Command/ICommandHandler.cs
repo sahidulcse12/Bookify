44261cb0eace6c4e355fc14864d875c5d92cc51b
sahidulcse12 <m.sahidulislam.14@gmail.com>
2024-08-03 15:11:16 +0600

    fix : partially implemented application layer

---
+++ Bookify.Application/Abstractions/Messaging/Command/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Messaging.Command
+{
+    public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result>
+        where TCommand : ICommand
+    {
+
+    }
+
+    public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+        where TCommand : ICommand<TResponse>
+    {
+
+    }
+}
---
+++ Bookify.Application/Abstractions/Messaging/Queries/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Bookify.Domain.Abstractions;
+
+namespace Bookify.Application.Abstractions.Messaging.Queries
+{
+    public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+        where TQuery : IQuery<TResponse>
+    {
+    }
+}
